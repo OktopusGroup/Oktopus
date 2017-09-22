162e9d8be5426b990c5dba7899c702ad05bd076c
Synapse709 <synapse709@gmail.com>
2017-09-22 10:52:59 +0900

    Completed /newSite modal

--- src/components/form_components/modal/modal_newSite/index.js
+++ src/components/form_components/modal/modal_newSite/index.js
@@ -32,29 +32,26 @@ class ModalNewSite extends React.PureComponent {
           <p className={css.title}>Add a custom domain</p>
           <div className={css.subtitle}>Create a...</div>
 
-          <div className={css.box_sharing}>
+          <div className={css.boxes}>
 
-            <p className={css.description}>Content and file sharing community</p>
-            <button className={css.btn_blue_large}>Begin</button>
-          
-          </div>
+            <div className={css.box_sharing}>
+              <img src={share} className={css.box_image} alt="create a freelancer platform" />
+              <p className={css.description}>Content and file sharing community</p>
+              <button className={css.btn_blue_large}>Begin</button>
+            </div>
 
-          <div className={css.box_hiring}>
-            <img src={hire} className={css.box_image} alt="create a freelancer platform" />
-            <p className={css.description}>Hiring platform for freelancers</p>
-            <div className={css.btn_blue_large_disabled}>Coming Soon</div>
-          </div>
+            <div className={css.box_hiring}>
+              <img src={hire} className={css.box_image} alt="create a freelancer platform" />
+              <p className={css.description}>Hiring platform for freelancers</p>
+              <div className={css.btn_blue_large_disabled}>Coming Soon</div>
+            </div>
 
-          <div className={css.box_hiring}>
-            <img src={hire} className={css.box_image} alt="create a freelancer platform" />
-            <p className={css.description}>Marketplace for digital goods</p>
-            <div className={css.btn_blue_large_disabled}>Coming Soon</div>
-          </div>
+            <div className={css.box_marketplace}>
+              <img src={marketplace} className={css.box_image} alt="create an online marketplace" />
+              <p className={css.description}>Connect an existing domain</p>
+              <div className={css.btn_blue_large_disabled}>Coming Soon</div>
+            </div>
 
-          <div className={css.box_marketplace}>
-            <img src={marketplace} className={css.box_image} alt="create an online marketplace" />
-            <p className={css.description}>Connect an existing domain</p>
-            <div className={css.btn_blue_large_disabled}>Coming Soon</div>
           </div>
 
         </div>
