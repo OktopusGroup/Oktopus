80f9eaa9a43381eb419fbc623f960db57562b98c
Synapse709 <synapse709@gmail.com>
2017-09-25 22:04:42 +0900

    Changes made to test/editor

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -8,6 +8,7 @@ import cn from 'classnames';
 import Helmet from 'react-helmet';
 import HeaderLoggedIn from 'components/header/loggedIn';
 import EditorButtonBar from 'components/editor/buttonBar';
+import EditorCheckBox from '../form/button/checkbox';
 
 /* Local */
 
@@ -38,17 +39,16 @@ class EditorFrame extends React.PureComponent {
         <div className={css.wrapper_editor}>
 
           <div className={css.sidebar}>
-            <h4>Add / Remove Features</h4>
-            <ul>
-              <li><a href="/#">Sort By   X</a></li>
-              <li><a href="/#">Categories   X</a></li>
-              <li><a href="/#">another feature   X</a></li>
-              <button>Choose Categories</button>
-            </ul>
+            <span className={css.sidebar_title}>Add / Remove Features</span>
+
+            <div className={css.grey_box}><span className={css.list_item} href="/#">Sort By <div className={css.align_checkbox}><EditorCheckBox /></div></span></div>
+
+            <div className={css.grey_box}><span className={css.list_item} href="/#">Categories</span></div>
+            <EditorCheckBox />
+            <div className={css.grey_box}><span className={css.list_item} href="/#">another feature</span></div>
+            <EditorCheckBox />
+
           </div>
-
-          <div className={css.workspace}>WORKSPACE</div>
-
         </div>
 
       </div>
