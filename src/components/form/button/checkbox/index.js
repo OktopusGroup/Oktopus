f88e6e5d90c7c213adb0cde7a0fd09143b9757e2
Lee Benson <lee@leebenson.com>
2017-09-26 10:46:47 +0100

    checkbox fix

--- src/components/form/button/checkbox/index.js
+++ src/components/form/button/checkbox/index.js
@@ -3,6 +3,8 @@
 
 /* NPM */
 import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
 
 /* Local */
 
@@ -14,17 +16,36 @@ import css from './checkbox.scss';
 // Create component for button
 
 class EditorCheckBox extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    onChange: PropTypes.func,
+    isChecked: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    className: null,
+    style: null,
+    onChange: null,
+    isChecked: false,
+  }
+
   submit = () => {
     /* intentionally empty for now -- the future home of our submit function */
   }
 
 
   render() {
+    const { style, className, onChange, isChecked } = this.props;
+    const classes = cn(css.wrapper, className);
     return (
-
-      <div className={css.wrapper}>
+      <div className={classes} style={style} >
         <div className={css.checkbox}>
-          <input defaultChecked className={css.checkbox_input} type="checkbox" />
+          <input
+            className={css.checkbox_input}
+            type="checkbox"
+            checked={isChecked}
+            onChange={onChange} />
           <svg className={css.checkbox_svg} viewBox="0 0 100 100">
             <polyline className={css.checkbox_polyline} points="83.1,31.5 39,75.5 16.9,53.5" />
           </svg>
