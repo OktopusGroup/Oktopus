9e965eaa91dfbfc09069eff09221b919d36a2c26
Synapse709 <synapse709@gmail.com>
2017-10-01 22:36:41 +0900

    Playin with component grid

--- src/components/test/index.js
+++ src/components/test/index.js
@@ -5,16 +5,19 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 // Components to test
-import ArrowButton from 'components/form/button/arrow';
 import HeaderLoggedIn from 'components/header/loggedIn';
-import Grid from './grid';
+import EditorButtonBar from 'components/editor/buttonBar';
+import EditorSidebarLogin from 'components/editor/sidebar/login';
+import EditorPreviewBar from 'components/editor/previewBar';
+
+import Grid from './grid/editor';
 
 // Styles
 import css from './test.scss';
 
 // Grid cell
 const Cell = props => (
-  <div className={props.area} style={{ backgroundColor: props.color }} />
+  <div>{props.children}</div>
 );
 
 Cell.propTypes = {
@@ -24,24 +27,13 @@ Cell.propTypes = {
 
 export default () => (
   <div className={css.container}>
-    <h2>Links</h2>
-    <Link to="/mySites"><img src="https://media.giphy.com/media/13xHqoOQOdFu5a/giphy.gif" alt="Link me" /></Link>
-    <hr />
-    <h2>Arrow button</h2>
-    <ArrowButton direction="up">Up</ArrowButton>
-    <ArrowButton direction="down">Down</ArrowButton>
-    <ArrowButton direction="left">Left</ArrowButton>
-    <ArrowButton direction="right">Right</ArrowButton>
-    <ArrowButton>Default</ArrowButton>
-    <ArrowButton className={css.arrowStyles}>Styled</ArrowButton>
-    <hr />
     
     <h2>Grid</h2>
     <Grid
 
-      A={props => <Cell area={props.area} color="red" />}
-      B={props => <Cell area={props.area} color="blue" />}
-      C={props => <Cell area={props.area} color="orange" />}
-      D={props => <Cell area={props.area} color="silver" />} />
+      A={props => <Cell area={props.area}><HeaderLoggedIn /></Cell>}
+      B={props => <Cell area={props.area}><EditorButtonBar /></Cell>}
+      C={props => <Cell area={props.area}><EditorPreviewBar /></Cell>}
+      D={props => <Cell area={props.area}><EditorSidebarLogin /></Cell>} />
   </div>
 );
